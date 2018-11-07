b3672b260af22ad182eee7bd12bc08d75cfe8cea
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-07 14:10:13 -0500

    urlIfy is done

---
+++ array.js
@@ -0,0 +1,93 @@
+const Memory = require('./memory');
+const memory = new Memory();
+
+
+// get(ptr), set(ptr, value), allocate(size), free(ptr), 
+//   copy(to, from, size)
+
+// [4, 2]
+
+//             c=2
+//             l=2
+// xx123xx42fdh42xxfsxxxggg
+//             p
+// Push(3)
+
+//             c=9
+//             l=3
+// xx123xx42fdh42xxfsxxxggg4635x423xxxxxxxxxxxxxxxxxxxxxx
+//                              p
+
+// pop ()
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this.ptr = memory.allocate(this.length);
+    this._capacity = 0;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    //WE FORGOT THIS!!!
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length){
+      throw new Error('Index is out of bounds');
+    }
+
+    return memory.get(this.ptr + index);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize( (this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  pop() {
+    //FORGOT THIS
+    if (this.length == 0) {
+      throw new Error('Index error');
+    }
+
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length){
+      throw new Error('Index is out of bounds');
+    }
+    if (this.length >= this._capacity) {
+      this._resize( (this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1,this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+
+    // WE FORGOT THIS!
+    this.length++;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length){
+      throw new Error('Index is out of bounds');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index -1);
+    this.length--;
+  }
+
+}
+module.exports = Array;
--- drills.js
+++ drills.js
@@ -1,96 +1,6 @@
 'use strict';
 
-const Memory = require('./memory');
-const memory = new Memory();
-
-// get(ptr), set(ptr, value), allocate(size), free(ptr), 
-//   copy(to, from, size)
-
-// [4, 2]
-
-//             c=2
-//             l=2
-// xx123xx42fdh42xxfsxxxggg
-//             p
-// Push(3)
-
-//             c=9
-//             l=3
-// xx123xx42fdh42xxfsxxxggg4635x423xxxxxxxxxxxxxxxxxxxxxx
-//                              p
-
-// pop ()
-
-class Array {
-  constructor() {
-    this.length = 0;
-    this.ptr = memory.allocate(this.length);
-    this._capacity = 0;
-  }
-
-  _resize(size) {
-    const oldPtr = this.ptr;
-    this.ptr = memory.allocate(size);
-    //WE FORGOT THIS!!!
-    if (this.ptr === null) {
-      throw new Error('Out of memory');
-    }
-
-    memory.copy(this.ptr, oldPtr, this.length);
-    memory.free(oldPtr);
-    this._capacity = size;
-  }
-
-  get(index) {
-    if (index < 0 || index >= this.length){
-      throw new Error('Index is out of bounds');
-    }
-
-    return memory.get(this.ptr + index);
-  }
-
-  push(value) {
-    if (this.length >= this._capacity) {
-      this._resize( (this.length + 1) * Array.SIZE_RATIO);
-    }
-    memory.set(this.ptr + this.length, value);
-    this.length++;
-  }
-
-  pop() {
-    //FORGOT THIS
-    if (this.length == 0) {
-      throw new Error('Index error');
-    }
-
-    const value = memory.get(this.ptr + this.length - 1);
-    this.length--;
-    return value;
-  }
-
-  insert(index, value) {
-    if (index < 0 || index >= this.length){
-      throw new Error('Index is out of bounds');
-    }
-    if (this.length >= this._capacity) {
-      this._resize( (this.length + 1) * Array.SIZE_RATIO);
-    }
-    memory.copy(this.ptr + index + 1,this.ptr + index, this.length - index);
-    memory.set(this.ptr + index, value);
-
-    // WE FORGOT THIS!
-    this.length++;
-  }
-
-  remove(index) {
-    if (index < 0 || index >= this.length){
-      throw new Error('Index is out of bounds');
-    }
-    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index -1);
-    this.length--;
-  }
-
-}
+const Array = require('./array');
 
 function main(){
 
@@ -114,4 +24,24 @@ function main(){
   console.log(arr.get(0));
 }
 
-main();
+// main();
+
+// URLify a String
+function urlIfy(str) {
+  const strArray = str.split('');
+  let newStrArray = [];
+  //foreach letter
+  //if letter = ' ', replace with %20
+
+  strArray.forEach( char => {
+    if ( char === ' ') {
+      newStrArray.push('%20');
+    } else {
+      newStrArray.push(char);
+    }
+  });
+
+  return newStrArray.join('');
+}
+
+console.log(urlIfy('this is A test '));
