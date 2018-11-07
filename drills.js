6e254f10973da52de8a9f5bacdcf3fe8d3a6188b
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-07 10:45:56 -0700

    finished array class

--- drills.js
+++ drills.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const memory = require('./memory');
+const Memory = require('./memory');
+const memory = new Memory();
+
 // get(ptr), set(ptr, value), allocate(size), free(ptr), 
 //   copy(to, from, size)
 
@@ -21,23 +23,22 @@ const memory = require('./memory');
 
 class Array {
   constructor() {
-    this.ptr = memory.allocate(this.length);
     this.length = 0;
+    this.ptr = memory.allocate(this.length);
     this._capacity = 0;
   }
 
   _resize(size) {
     const oldPtr = this.ptr;
     this.ptr = memory.allocate(size);
-    
     //WE FORGOT THIS!!!
     if (this.ptr === null) {
       throw new Error('Out of memory');
     }
 
     memory.copy(this.ptr, oldPtr, this.length);
-    this._capacity = size;
     memory.free(oldPtr);
+    this._capacity = size;
   }
 
   get(index) {
@@ -68,12 +69,43 @@ class Array {
   }
 
   insert(index, value) {
+    if (index < 0 || index >= this.length){
+      throw new Error('Index is out of bounds');
+    }
+    if (this.length >= this._capacity) {
+      this._resize( (this.length + 1) * Array.SIZE_RATIO);
+    }
+    memory.copy(this.ptr + index + 1,this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
 
+    // WE FORGOT THIS!
+    this.length++;
   }
 
   remove(index) {
-
+    if (index < 0 || index >= this.length){
+      throw new Error('Index is out of bounds');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index -1);
+    this.length--;
   }
 
 }
-Array.SIZE_RATIO = 3;
+
+function main(){
+
+  Array.SIZE_RATIO = 3;
+
+  //create an instance of the array class
+  let arr = new Array();
+  //add an item to the array
+  arr.push(3);
+  arr.push(5);
+  arr.push(7);
+  arr.get(0);
+  arr.pop();
+
+  console.log(arr, arr.get(0), arr.pop());
+}
+
+main();
